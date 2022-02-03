52f991426ecb058db0e218d17ea023b50c58d071
Trevor Stuart <stuart.trev@gmail.com>
2022-02-03 00:06:13 -0500

    GrowthStage enums

---
+++ Nukleo.Data/Enum/GrowthStage.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nukleo.Data.Enum
+{
+    public enum CornGrowthStage
+    {
+        [Display(Name = " ")]
+        blank,
+        V0,
+        VE,
+        V1,
+        V2,
+        V3,
+        V4,
+        V5,
+        V6,
+        V7,
+        V8,
+        V9,
+        V10,
+        V11,
+        V12,
+        V13,
+        V14,
+        V15,
+        VT,
+        R1,
+        R2,
+        R3,
+        R4,
+        R5,
+        R6
+    }
+
+    public enum SoyGrowthStage
+    {
+        [Display(Name = " ")]
+        blank,
+        VE,
+        VC,
+        V1,
+        V2,
+        V3,
+        V4,
+        V5,
+        V6,
+        V7,
+        V8,
+        V9,
+        V10,
+        V11,
+        V12,
+        V13,
+        V14,
+        V15,
+        R1,
+        R2,
+        R3,
+        R4,
+        R5,
+        R6,
+        R7,
+        R8
+    }
+}
---
+++ Nukleo.Data/Enum/Month.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nukleo.Data.Enum
+{
+    public enum Month
+    {
+        January = 1,
+        February,
+        March,
+        April,
+        May,
+        June,
+        July,
+        August,
+        September,
+        October,
+        November,
+        December
+    }
+}
