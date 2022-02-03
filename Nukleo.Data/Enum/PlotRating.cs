377054e2183ff9f66d4d9a8da310ddbf7566974b
Trevor Stuart <stuart.trev@gmail.com>
2022-02-02 21:06:10 -0500

    add data folders and enums

---
+++ Nukleo.Data/Enum/PlotRating.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nukleo.Data.Enum
+{
+    public enum PlotRating
+    {
+        [Display(Name = " ")]
+        NoRating,
+        Excellent = 5,
+        [Display(Name = "Above Average")]
+        AboveAverage = 4,
+        Average = 3,
+        [Display(Name = "Below Average")]
+        BelowAverage = 2,
+        Poor = 1
+    }
+}
+
+
