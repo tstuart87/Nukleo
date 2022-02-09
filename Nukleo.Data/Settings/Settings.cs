573be931038930c0b96652966e8be12beae7a7b1
Trevor Stuart <stuart.trev@gmail.com>
2022-02-09 16:30:25 -0500

    Settings update

--- Nukleo.Data/Settings/Settings.cs
+++ Nukleo.Data/Settings/Settings.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,8 @@ namespace Nukleo.Data.Settings
 {
     public class Settings
     {
-
+        [Key]
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
     }
 }
